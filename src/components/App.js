728afd35d3b5777fa782d182e61af7637fb3a2c0
Kevin <kevinqiyf@hotmail.com>
2019-04-20 17:03:06 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Route path="/" render={() => <Home />} />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
