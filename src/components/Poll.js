6b383db067048f7566b93c151a0292fc59c69639
Kevin <kevinqiyf@hotmail.com>
2019-06-03 15:59:09 -0700

    finished the poll logic

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -17,11 +17,28 @@ const Poll = () => {
     fetchPoll();
   }, []);
 
+  const addCount = (count, id) => {
+    fetch(`${URL_HOME}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ count: count + 1 })
+    }).then(() => {
+      fetchPoll();
+    });
+  };
+
   const renderPoll = () => {
     const positions = ['1ST', '2ND', '3RD'];
 
     return pollTeams.map((item, index) => (
-      <div key={item.id} className="poll_item">
+      <div
+        key={item.id}
+        className="poll_item"
+        onClick={() => addCount(item.count, item.id)}
+      >
         <img alt={item.name} src={`/images/teams/${item.logo}`} />
         <h4>{positions[index]}</h4>
         <div>{item.count} Votes</div>
